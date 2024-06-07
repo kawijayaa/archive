24ee6fbf0c7c545c3484bb075e9df50bd764d27f
Muhammad Oka <oka@muhammadoka.dev>
2024-06-07 23:51:13 +0700

    add archive page

---
+++ main/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-around p-8 md:text-3xl">
+      <a href="https://v1.archive.muhammadoka.dev" className="hover:underline hover:text-cyan-500">v1</a>
+      <a href="https://v2.archive.muhammadoka.dev" className="hover:underline hover:text-cyan-500">v2</a>
+      <a href="https://v3.archive.muhammadoka.dev" className="hover:underline hover:text-cyan-500">v3</a>
+      <a href="https://v4.archive.muhammadoka.dev" className="hover:underline hover:text-cyan-500">v4</a>
+      <a href="https://v5.archive.muhammadoka.dev" className="hover:underline hover:text-cyan-500">v5</a>
+      <a href="https://muhammadoka.dev" className="hover:underline hover:text-cyan-500">current</a>
+    </main>
+  );
+}
